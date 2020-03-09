8199c22cdda5cf5206c635d2bdde3c772775eb4a
Lauren Yu <laurenkyu@gmail.com>
2020-03-09 14:20:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,4 +18,14 @@ document.addEventListener("keydown", function(e) {
 function moveDodgerRight() {
   let rightNumbers = dodger.style.right.replace("px", "");
   let right = parseInt(rightNumbers, 10);
+  
+  if (right < 360) {
+    dodger.style.right = `${right - 1}px`;
+  }
 }
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
