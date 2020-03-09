cb6beeb70b8b083bd18435aa3ea51edea904a38e
Lauren Yu <laurenkyu@gmail.com>
2020-03-09 14:24:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
   }
-  else if (e.key === "ArrowRight"){
+  else if (e.key === "ArrowRight") {
     moveDodgerRight();
   }
 });
